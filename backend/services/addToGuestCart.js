3cc0006d5db69447a26082f3b489ab5cf60668e1
JakubBilski1 <jakub.bilski@spoko.pl>
2023-11-24 00:48:58 +0100

    adding to guest cart function

--- backend/services/addToGuestCart.js
+++ backend/services/addToGuestCart.js
@@ -1,21 +1,42 @@
 const dbConnect = require('./dbConnect');
+
 const addToGuestCart = async (product, sessionId) => {
-    const db = await dbConnect();
-    const cartCol = db.collection('CartForGuests');
-    const query = { userId: sessionId };
-    const cart = await cartCol.findOne(query);
-    if(!cart){
-        const cartData = {
-            userId: sessionId,
-            cart: [product]
+    try{
+        const db = await dbConnect();
+        const cartCol = db.collection('CartForGuests');
+        const query = { userId: sessionId };
+        const cart = await cartCol.findOne(query);
+        let isItemInCart = false;
+        if(!cart){
+            const cartData = {
+                userId: sessionId,
+                cart: [product],
+                expiresAfter: new Date(Date.now() + 60 * 1000).toLocaleString()
+            }
+            const result = await cartCol.insertOne(cartData);
+            return result;
+        }else{
+            if (product.id && product.size) {
+                cart.cart.forEach((item) => {
+                    if (item.id == product.id && item.size == product.size) {
+                        isItemInCart = true;
+                    }
+                });
+                if (isItemInCart) {
+                    return 'This item is already in your cart';
+                } else {
+                    const update = { $push: { cart: product }, $set: { expireAt: new Date(Date.now() + 60 * 1000) } };
+                    const result = await cartCol.updateOne(query, update);
+                    return result;
+                }
+            } else {
+                return 'Set cart';
+            }
         }
-        const result = await cartCol.insertOne(cartData);
-        return result;
-    }else{
-        const update = { $push: { cart: product } };
-        const result = await cartCol.updateOne(query, update);
-        return result;
+    }catch(err){
+        console.log(err)
     }
+    
 }
 
 module.exports = addToGuestCart;
