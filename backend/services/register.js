38a0e0631e9913c9f9f16e414fca86ddd49f3ed0
JakubBilski1 <jakub.bilski@spoko.pl>
2023-11-25 23:15:52 +0100

    register cart remember (sessionID to fix)

--- backend/services/register.js
+++ backend/services/register.js
@@ -2,22 +2,40 @@ const argon2 = require('argon2');
 const dbConnect = require('./dbConnect');
 
 const register = async (req, res) => {
+    const sessionId = req.sessionID;
+    console.log(sessionId)
+    let result, guestCart
+    const cartToSend = []
     const db = await dbConnect();
     const password = await argon2.hash(req.body.data.password);
     const { email, nick } = req.body.data;
     const collection = db.collection('Users');
+    const guestCol = db.collection('CartForGuests');
+    const guestQuery = {userId: sessionId};
+    const guestArr = await guestCol.find(guestQuery).toArray();
+    if(guestArr.length !== 0){
+      guestCart = guestArr[0].cart
+      guestCart.forEach((item) => {
+        cartToSend.push(item)
+      })
+    }
     const query = { email: email };
     const user = await collection.findOne(query);
     if (user) {
       res.status(409).json({ error: 'User already exists' });
     } else {
-      const result = await collection.insertOne({ email, password, nick, settings: {darkTheme: true, country: '', city: '', phoneNumber: '', birthDate: ''}, profileImage: '', cart: [], orders: [], role: 'user', createdAt: new Date(), favTeams: [] });
+      if(guestArr.length === 0){
+        await collection.insertOne({ email, password, nick, settings: {darkTheme: true, country: '', city: '', phoneNumber: '', birthDate: ''}, profileImage: '', cart: [], orders: [], role: 'user', createdAt: new Date(), favTeams: [] })
+      }else{
+        await collection.insertOne({ email, password, nick, settings: {darkTheme: true, country: '', city: '', phoneNumber: '', birthDate: ''}, profileImage: '', cart: cartToSend, orders: [], role: 'user', createdAt: new Date(), favTeams: [] });
+      }
       const userData = {
         id: result.insertedId,
         email,
         password,
         nick
       }
+
       res.json({ userData, redirectTo: '/login' });
     }
 }
